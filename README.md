262db514bac19f1d76c6df58b1eeaf81620e9478
Mike Pettman <mp_palmatum@yahoo.com>
2017-09-16 09:28:11 -0500

    Append info to DOM in table form. Add complete and delete buttons.

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ Base mode:
 - [x] Set up database (add to database.sql).
 - [x] Connect database using pool.
 - [x] Retrieve get array.
-- [ ] Set up POST req to DB.
-- [ ] Append info to DOM.
-- [ ] Add delete button. Add DELETE req to server.
-- [ ] Add complete button. Add styles folder and file, and add bg-color classes.
+- [x] Set up POST req to DB.
+- [x] Append info to DOM in table form. Add complete and delete buttons.
+- [ ] Add event listeners for complete and delete task buttons.
+- [ ] Add DELETE req to server.
+- [ ] Add styles folder and file, and add bg-color classes.
 - [ ] Use PUT req to update boolean value of completed?
 - [ ] Style page. Bootstrap?
 
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,21 +1,24 @@
 $(document).ready(onReady);
 
-function onReady(){
+function onReady() {
     getList();
     $('#submitButton').on('click', addTask);
 }
 
-function getList(){
+// GET req to server to obtain array of tasks
+function getList() {
     $.ajax({
         method: 'GET',
         url: '/tasks',
-        success: function(res){
+        success: function (res) {
             console.log(res);
+            appendTasks(res);
         }
     });
-}
+}   // end getList func
 
-function addTask(){
+// POST req to server to add a task
+function addTask() {
     var $task = $('#taskIn').val();
     // clear value
     $('#taskIn').val('');
@@ -23,12 +26,24 @@ function addTask(){
     $.ajax({
         method: 'POST',
         url: '/tasks',
-        data : {
+        data: {
             task: $task
         },
-        success: function(res){
+        success: function (res) {
             console.log(res);
         }
     });
     getList();
-}
+} // end addTask funct
+
+// append info to DOM
+function appendTasks(taskArray) {
+    $('#container').empty();
+    for (var i in taskArray) {
+        var $row = $('#container').append('<tr></tr>');
+        $row.append('<td>' + taskArray[i].task + '</td>');
+        $row.append('<td><button class="completeButton">Complete</button></td>');
+        $row.append('<td><button class="deleteButton">Delete</button></td>');
+        $('#container').append($row);
+    }
+}   // end appendTasks funct
